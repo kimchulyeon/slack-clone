5298fb90972fdb2f818734b7dd28cf6aaf67c0dd
KimChulyeon <guinness987@gmail.com>
2022-06-28 17:59:30 +0900

    코드스플릿팅

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import loadable from '@loadable/component';
+//=====================o코드스플릿팅o===========================
+const Login = loadable(() => import('@pages/login/Login'));
+const Signup = loadable(() => import('@pages/signup/Signup'));
+//============================================================
 
 const App = () => {
-  return <div>hello world!!!!!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 };
 
 export default App;
